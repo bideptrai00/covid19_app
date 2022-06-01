5dcf4654a5fabe15b795fc252a4cc5e37653ab6a
HieuLe <lehieugb00@gmail.com>
2022-06-01 23:41:09 +0700

    Push

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const getCountries = () =>
-  axios.get("https://api.covid19api.com/countries");
+export const getCountries = () => axios.get(" /countries");
 
-export const getReportByCountry = (country ) =>
+export const getReportByCountry = (country) =>
   axios.get(`https://api.covid19api.com/dayone/country/${country}`);
 
 export const getMapById = (countyID) =>
-  axios.get(`http://code.highcharts.com/mapdata/countries/${countyID}/${countyID}-all.geo.json`);
-
+  axios.get(
+    `http://code.highcharts.com/mapdata/countries/${countyID}/${countyID}-all.geo.json`
+  );
